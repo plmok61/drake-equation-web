7d3fb44bff674c667c382980427ded371635d13d
PHILIP MOK <plmok61@gmail.com>
2021-06-17 09:19:20 -0500

    new app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
+import Privacy from './Privacy';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Switch>
+      <Route path="/privacy" exact>
+        <Privacy />
+      </Route>
+      <Route path="/">
+        <div className="App">
+          <div className="centerBody">
+            <div className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>Drake Equation</h2>
+            </div>
+            <h4>
+              Calculate the number of alien civilizations in our galaxy
+            </h4>
+          </div>
+        </div>
+      </Route>
+    </Switch>
+  </Router>
   );
 }
 
